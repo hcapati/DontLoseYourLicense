0babb09db97923ab8fd899d40f60befb6b14901e
Heather Capati <heathercapati@Heathers-Air.attlocal.net>
2019-02-04 09:55:34 -0800

    Don't Lose Your License Final

---
+++ front-end/src/constants/index.js
@@ -0,0 +1,14 @@
+export const ADD_USER           = 'ADD_USER'; 
+export const DELETE_USER        = 'DELETE_USER';
+export const EDIT_USER          = 'EDIT_USER';
+export const GET_USERS          = 'GET_USERS';
+export const GET_USER_TOKEN     = 'GET_USER_TOKEN';
+export const GET_USER_BY_ID     = 'GET_USER_BY_ID';
+export const SAVE_AVATAR        = 'SAVE_AVATAR';
+export const LOGOUT_USER        = 'LOGOUT_USER';
+
+export const ADD_ARTICLE        = 'ADD_ARTICLE';
+export const DELETE_ARTICLE     = 'DELETE_USER';
+export const EDIT_ARTICLE       = 'EDIT_ARTICLE';
+export const GET_ARTICLES       = 'GET_ARTICLES';
+
---
+++ front-end/src/stores/store.js
@@ -0,0 +1,88 @@
+import {
+    ADD_USER, 
+    DELETE_USER,
+    EDIT_USER, 
+    GET_USERS,
+    GET_USER_BY_ID,
+    ADD_ARTICLE,
+    DELETE_ARTICLE,
+    EDIT_ARTICLE,
+    GET_ARTICLES,
+    GET_USER_TOKEN,
+    SAVE_AVATAR,
+    LOGOUT_USER,
+
+} from '../constants';
+
+const initialState = {
+    loggedUser: {},
+    users: [],
+    articles: null,
+    isLogged: false
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_USER:
+        return {
+            ...state,
+            loggedUser: action.payload,
+            isLogged: true
+        }
+        case GET_USER_BY_ID: 
+        return {
+            ...state,
+            loggedUser: action.payload
+        }
+        case GET_USERS:
+        return {
+            ...state,
+            users: action.payload
+        }
+        case GET_USER_TOKEN:
+            localStorage.setItem('token', action.token);
+            return {
+                ...state,
+                isLogged: true
+            }
+        case DELETE_USER:
+        return {
+            ...state,
+            users: action.payload
+        }
+        case SAVE_AVATAR:
+        return {
+            ...state,
+            loggedUser: {
+                ...state.loggedUser,
+                avatar: action.avatar
+            }
+        }
+        case LOGOUT_USER:
+        return {
+            ...state,
+            isLogged: false,
+            loggedUser: {}
+        }
+        case ADD_ARTICLE:
+        return {
+            ...state,
+            articles: action.payload
+        }
+        case GET_ARTICLES:
+        console.log(action.payload);
+        return {
+            ...state,
+            articles: action.payload
+        }
+        case DELETE_ARTICLE:
+        return {
+            ...state,
+            articles: action.payload
+        }
+        default:
+        return state;
+    }
+}
+
+export default rootReducer;
